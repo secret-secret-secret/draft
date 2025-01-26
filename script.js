4d6b95113c48769a7091c3509faf6a5c68cc158c
secret-secret-secret <hellishshellfish@gmail.com>
2025-01-26 09:25:26 -0500

    source veiwer and downloader functions

--- script.js
+++ script.js
@@ -1,26 +1,44 @@
-function readFile(file) {
-   filetype;
-   source;
-   sendToReader(filetype, source)
+function welcome() {
+   alert("Welcome. Keep your prints, faces, and IPs outside of the vehicle");
 }
 
-function sendToReader(filetype, source) {
+// file viewer and downloader functions
+function selectFile(source) {
+   // TODO: go to the last character in the file, traverse backwards,
+   // find the file extension, copy it to the variable "extension"
+   for (let periodFinder = source.length; periodFinder > source.length; periodFinder--) {
+      if (source[index] = ".") {
+       for (let extensionFinder = 0; extensionFinder < periodFinder; extensionFinder++) {
+         extension = extension + array[index];
+       }  
+      break;
+      }
+   }
+   extension;
+   sendToViewer(source)
+   sendToDownloader(sourcee)
+}
+
+function sendToViewer(extension, source) {
    viewer = document.getElementById(viewer);
    // clear class, src, and dimesnion value
    viewer.innerHTML = null;
-   // find a way to assign and pass values through if statement
-   viewer.innerHTML = 'class="' + filetype + ' src="' + source + '"';
+   viewer.innerHTML = 'type="' + extension + ' href="' + source + '" height="500em" width="100%"';
 }
+
+function sendToDownloader(source){
+   downloader = document.getElementById("downloader");
+   downloader.innerHTML = null;
+   downloader.innerHTML = 'id="downloader" href="' + source + '" download';
+}
+
+// downloading files
 
 function download(link) {
    // https://drive.google.com/file/d/1LDRlyZaZTF-bxHjBDEiL2obMxYHtf3YT/view?usp=sharing
    // And it extracts the file ID, the part between /d/ and /view, and puts it into a new URL which looks like this:
    // https://drive.google.com/uc?export=download&id=1LDRlyZaZTF-bxHjBDEiL2obMxYHtf3YT
 
-}
-
-function message() {
-   alert("Welcome. Keep your prints, faces, and IPs outside of the vehicle");
 }
 
 function nav(file) {
