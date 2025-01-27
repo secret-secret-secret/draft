cf3bccd2f11fc3d66284a27ebbc69fb65ec533ed
secret-secret-secret <hellishshellfish@gmail.com>
2025-01-27 17:32:44 -0500

    file navigation should work now

--- script.js
+++ script.js
@@ -9,22 +9,13 @@ function select(source) {
 }
 
 // showing file tables and file columns
-function tableVis(table) {
-   if (document.getElementById(table).hidden === "true") {
-      document.getElementById(table).hidden = "false";
-   } else {
-      document.getElementById(table).hidden = "true";
-   }
-}
-
-function columnVis(column) {
+function vis(column) {
    if (document.getElementById(column).style.visibility === "collapse") {
       document.getElementById(column).style.visibility = "visible";
    } else {
       document.getElementById(column).style.visibility = "collapse";
    }
 }
-
 // https://drive.google.com/file/d/1LDRlyZaZTF-bxHjBDEiL2obMxYHtf3YT/view?usp=sharing
 // And it extracts the file ID, the part between /d/ and /view, and puts it into a new URL which looks like this:
 // https://drive.google.com/uc?export=download&id=1LDRlyZaZTF-bxHjBDEiL2obMxYHtf3YT
