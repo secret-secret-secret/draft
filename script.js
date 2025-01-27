83d88f2ece1ccbe070192b9a37749d6f80cfaa01
secret-secret-secret <hellishshellfish@gmail.com>
2025-01-27 10:52:15 -0500

    table edits

--- script.js
+++ script.js
@@ -32,14 +32,10 @@ function sendToDownloader(source){
    downloader.innerHTML = 'id="downloader" href="' + source + '" download';
 }
 
-// downloading files
+// https://drive.google.com/file/d/1LDRlyZaZTF-bxHjBDEiL2obMxYHtf3YT/view?usp=sharing
+// And it extracts the file ID, the part between /d/ and /view, and puts it into a new URL which looks like this:
+// https://drive.google.com/uc?export=download&id=1LDRlyZaZTF-bxHjBDEiL2obMxYHtf3YT
 
-function download(link) {
-   // https://drive.google.com/file/d/1LDRlyZaZTF-bxHjBDEiL2obMxYHtf3YT/view?usp=sharing
-   // And it extracts the file ID, the part between /d/ and /view, and puts it into a new URL which looks like this:
-   // https://drive.google.com/uc?export=download&id=1LDRlyZaZTF-bxHjBDEiL2obMxYHtf3YT
-
-}
 
 function nav(file) {
    document.getElementById(file).hidden = false;
