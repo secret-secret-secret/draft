434c5aab12519d17ec2afb1abae3e4fdc9af7c88
secret-secret-secret <hellishshellfish@gmail.com>
2025-01-28 12:15:41 -0500

    current issue: cannot send file to viewing iframe from table

--- script.js
+++ script.js
@@ -4,15 +4,19 @@ function welcome() {
 
 // file viewer and downloader functions
 function select(source) {
-   document.getElementById("viewer").setAttribute("src", source);
-   document.getElementById("downloader").setAttribute("href", source)
+   index = index.html.URL;
+   index.getElementById("viewer").setAttribute("src", source);
+   index.getElementById("downloader").setAttribute("href", source)
 }
 
 // showing file tables and file columns
-function vis(column) {
-   if (document.getElementById(column).style.height === "25%") {
-      document.getElementById(column).style.height = "0";
-   } else {
-      document.getElementById(column).style.height = "25%";
-   }
+function vis(element) {
+   chosenElement = document.getElementById(element)
+   if (chosenElement.style.visibility === "visible") {
+      chosenElement.style.visibility = "collapse";
+      chosenElement.style.height = "0"
+    } else {
+      chosenElement.style.visibility = "visible";
+      chosenElement.style.height = "20em";
+    }
 }
