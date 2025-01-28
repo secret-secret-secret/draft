d1adbd570a24d1e05b3bef0228ffd58a540f2e78
secret-secret-secret <hellishshellfish@gmail.com>
2025-01-28 17:00:52 -0500

    good bye stupid fucking popup

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
-function welcome() {
-   alert("Welcome. Keep your prints, faces, and IPs outside of the vehicle");
-}
-
 // file viewer and downloader functions
 function select(source) {
-   index = index.html.URL;
-   index.getElementById("viewer").setAttribute("src", source);
-   index.getElementById("downloader").setAttribute("href", source)
+   document.getElementById("viewer").setAttribute("src", source);
+   document.getElementById("downloader").setAttribute("href", source)
 }
 
 // showing file tables and file columns
